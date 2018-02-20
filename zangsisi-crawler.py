b6c8e7a4896a3782d85f2c7b7c0a2856d643d89a
Gihyeon Yang <dexterastin@gmail.com>
2018-02-20 02:34:43 -0800

    크롤링시 멀티프로세싱 지원

--- zangsisi-crawler.py
+++ zangsisi-crawler.py
@@ -1,45 +1,70 @@
 import os
 import urllib
 import requests
-
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
+
+
+def unwrap_self_f(arg, **kwarg):
+    return zangsisiCrawler.get_contents(*arg, **kwarg)
 
 
 class zangsisiCrawler:
     def __init__(self, p):
+        self.directory = ""
+        self.links = []
         self.url = 'http://zangsisi.net/?p=' + str(p)
         pass
 
-    def crawling(self):
+    def get_links(self):
         req = requests.get(self.url)
         data = req.text
 
         soup = BeautifulSoup(data, 'html.parser')
         title = soup.select('.title')
 
-        dir = title[1].string
-        os.mkdir(dir)
+        self.directory = title[1].string
+        os.mkdir(self.directory)
 
         for link in soup.select('#post > div > p > a'):
-            page = BeautifulSoup(requests.get(link.get('href')).text, 'html.parser')
-
-            title = page.select('.title')[1].string
-            images = page.select('.contents > p > a')
-            count = 0
-
-            path = './' + dir + '/' + title
-            os.mkdir(path)
-
-            for image in images:
-                count += 1
-                imageUrl = image.get('href')
-                urllib.urlretrieve(imageUrl, path + '/' + os.path.basename(imageUrl))
-                pass
+            self.links.append(link.get('href'))
             pass
+        pass
+
+    def get_contents(self, link):
+        page = BeautifulSoup(requests.get(link).text, 'html.parser')
+
+        title = page.select('.title')[1].string
+        images = page.select('.contents > p > a')
+        count = 0
+
+        path = './' + self.directory + '/' + title
+        os.mkdir(path)
+
+        print("<< START >>\t" + title)
+
+        for image in images:
+            count += 1
+            imageUrl = urllib.quote(image.get('href').encode('utf8'), ':/')
+            urllib.urlretrieve(imageUrl, path + '/' + os.path.basename(imageUrl))
+            pass
+
+        print("<<  END  >>\t" + title)
+        pass
+
+    def run(self):
+        print("==== crawling start ====")
+
+        self.get_links()
+        pool = Pool(processes=4)
+        pool.map(unwrap_self_f, zip([self] * len(self.links), self.links))
+        pool.close()
+
+        print("==== crawling end ====")
         pass
 
 
 if __name__ == '__main__':
-    zangsisi = zangsisiCrawler(11962)
-    zangsisi.crawling()
+    zangsisi = zangsisiCrawler(11959)
+    zangsisi.run()
     pass
